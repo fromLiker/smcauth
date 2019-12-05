133bc4770296088504774870f3e8e24ce21c437b
xuexp <xuexp@cn.ibm.com>
2019-12-05 20:29:58 +0800

    confirm email link

--- src/main/java/com/ibm/fsdsmc/controller/UsersController.java
+++ src/main/java/com/ibm/fsdsmc/controller/UsersController.java
@@ -3,7 +3,7 @@ package com.ibm.fsdsmc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.ibm.fsdsmc.constant.Const;
@@ -17,16 +17,21 @@ import com.ibm.fsdsmc.utils.ResponseBean;
 
 import static org.springframework.http.HttpStatus.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @CrossOrigin
 @RestController
 @RequestMapping("smc/users")
 public class UsersController {
 
+  private static Logger logger = LoggerFactory.getLogger(UsersController.class);
+
   @Autowired
   private UsersService usersService;
   
-  @Autowired
-  private PasswordEncoder passwordEncoder;
+  // @Autowired
+  // private PasswordEncoder passwordEncoder;
   
   @Autowired
   private MailService mailService;
@@ -40,7 +45,8 @@ public class UsersController {
     	return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_ERROR_CODE, "User signup failed!"));
     
     String password = usersEntity.getPassword();
-    usersEntity.setPassword(passwordEncoder.encode(password));
+//    usersEntity.setPassword(passwordEncoder.encode(password));
+    usersEntity.setPassword(password);
     mailService.sendHTMLMail(usersInfo.getEmail(), usersInfo.getUsername());
     
     return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_OK_CODE, "User signup successfully!"));
@@ -48,12 +54,16 @@ public class UsersController {
   }
 
   @GetMapping("/confirmed/{username}")
-  public ResponseEntity<CommonResult> activeUserByUsername(@PathVariable("username") String username) throws Exception {
+  public String activeUserByUsername(@PathVariable("username") String username) throws Exception {
 	  // user click this link from email received to confirmed user
-	  if(usersService.setConfirmedByUsername(username, "1")>0)
-		  return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_OK_CODE, "User have confirmed!"));
-	  
-	  return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_ERROR_CODE, "User confirme action failed!"));
+	  if(usersService.setConfirmedByUsername(username, "1")>0) {
+//		  return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_OK_CODE, "User have confirmed!"));
+		  System.out.println("发送html文本文件-发生成功");
+		  logger.error("User have confirmed!");
+		  return "<a href='http://localhost:4200/login'>please clink here to login SMC system</a>";
+	  }
+	  return "User confirm action failed!";
+//	  return ResponseEntity.ok().body(CommonResult.build(Const.COMMONRESULT_ERROR_CODE, "User confirm action failed!"));
 //	  return ResponseEntity.ok().body(new ResponseBean(OK.value(), OK.getReasonPhrase()).data("User have confirmed!"));
   }
   
